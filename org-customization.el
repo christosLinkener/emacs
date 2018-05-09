c97719192f030c9191552938d6926ec5f3be23fd
actondev <chris.actondev@gmail.com>
2018-05-09 12:21:33 +0300

    enabling sh for org-babel

--- org-customization.el
+++ org-customization.el
@@ -7,6 +7,17 @@
 (setcar (nthcdr 1 org-emphasis-regexp-components) "[:alpha:]- \t.,:!?;'\")}\\")
 (org-set-emph-re 'org-emphasis-regexp-components org-emphasis-regexp-components)
 
+
+;; enabling sh code execution in org mode
+(org-babel-do-load-languages 'org-babel-load-languages
+    '(
+        (sh . t)
+    )
+)
+
+
+
+
 ;; fontifying source blocks
 (setq org-src-fontify-natively t
 	  org-src-preserve-indentation t
